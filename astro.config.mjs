20905612632ee48d80321a2db90eb7c43eb9a9ac
Lloyd Park <lloyd.park88@gmail.com>
2024-11-22 19:16:29 -0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import icon from 'astro-icon'
 import { SITE } from './src/config'
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
